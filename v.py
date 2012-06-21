707a5aecee9b47f80a2dc90b817ab406778d5c77
dmitry <dmitry@whelf.(none)>
2012-06-21 02:10:09 -0700

    Switching to isomap

--- v.py
+++ v.py
@@ -2,21 +2,76 @@ import datetime, os, time
 import numpy as np
 import pylab as pl
 
-#from calc_tsne import calc_tsne as tsne
-from tsne import tsne 
+from scipy.sparse import csr_matrix
+from sklearn.metrics.pairwise import pairwise_distances 
+from sklearn.decomposition import KernelPCA
+from sklearn.utils.graph import graph_shortest_path
+
+
+
+def kneighbors(X, n_neighbors, metric):
+     """Finds the K-neighbors of a point.
+     Returns distance
+    """
+     if metric == 'abs_correlation': 
+         dist = pairwise_distances(X, metric = 'correlation')
+         dist = np.sqrt( -np.log( np.abs(1 - dist )) )
+     else:     
+         dist = pairwise_distances(X, metric = metric)
+         
+         
+     neigh_ind = dist.argsort(axis=1)
+     neigh_ind = neigh_ind[:, :n_neighbors]
+     return neigh_ind
+
+    
+def kneighbors_graph(X, n_neighbors, metric):
+     """Computes the (weighted) graph of k-Neighbors for points in X
+     """
+     X = np.asarray(X)
+
+     n_samples1 = n_samples2 = X.shape[0]
+     n_nonzero = n_samples1 * n_neighbors
+     A_indptr = np.arange(0, n_nonzero + 1, n_neighbors)
+
+     # construct CSR matrix representation of the k-NN graph
+     A_data = np.ones((n_samples1, n_neighbors))
+     A_ind = kneighbors(X, n_neighbors, metric)
+
+     return csr_matrix((A_data.ravel(), A_ind.ravel(), A_indptr), shape=(n_samples1, n_samples2))
+
+
+def isomap(X, n_neighbors, metric):
+    kng = kneighbors_graph(D, n_neighbors = n_neighbors, metric = metric)    
+    dist_matrix_ = graph_shortest_path(kng, method='auto', directed=False)    
+    kernel_pca_ = KernelPCA(n_components=2, kernel="precomputed", eigen_solver='auto')
+    G = dist_matrix_ ** 2
+    G *= -0.5
+    return kernel_pca_.fit_transform(G)
+
+
 
 X = np.array( [pl.imread('frames/' + f).flatten() for f in sorted(os.listdir("frames"))], dtype = np.float64 )
 
 SW = np.random.permutation(X.shape[1])
 X = X[:, SW]     # permute pixels
 
-results = tsne(X.T)
-x,y = results.real[:,0], results.real[:,1]
 
-pl.scatter(x, y, c = np.arange(X.shape[1])[SW], cmap=pl.jet())
-pl.show()
+#D = np.array([np.concatenate((pixel, pixel[i%2:-i%2 or None], pixel[(i+1)%2:(i+1)%2 or None])) for i,pixel in enumerate(X.T)])
+D = X.T
 
+for metric in ['abs_correlation', 'braycurtis', 'canberra', 'correlation', 'cosine', 'minkowski', 'seuclidean']:
+  for n_neighbors in [4,5,6]:
+    pl.figure()
+    results = isomap(D, n_neighbors, metric = metric)
+    x,y = results.real[:,0], results.real[:,1]  
+    pl.scatter(x, y, c = np.arange(X.shape[1])[SW], cmap=pl.jet())
+    pl.title("%s n_neighbors %d" % (metric, n_neighbors))
+    pl.savefig("%s.n_neighbors.%d.png" % (metric, n_neighbors))
+    #pl.show()
+    
 
+exit(0)
 print "Saving restored frames..."
 
 name = "movie"    
